400a98f37bae76cd8b5246e13a4de3277957bc42
Roman <maryushkinroman91@yandex.ru>
2022-07-26 22:15:24 +0300

    addNewsListComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import NewsList from './pages/NewsList/NewsList';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<NewsList />} />
+      </Routes>
     </div>
   );
 }
